f62671e77810d8428623c3933d705455c4c34209
Magyari-Sáska Attila <atti2002@gmail.com>
2024-08-12 12:02:26 +0300

    Added basic fucntionality to handler

--- codes.py
+++
@@ -1,3 +0,0 @@
-from xrds_handler import XRDS_handler
-
-ds = XRDS_handler('./data/tg_ens_mean_0.1deg_reg_2011-2023_v29.0e.nc')
--- xrds_handler.py
+++ xrds_handler.py
@@ -2,6 +2,10 @@ import xarray as xr
 
 class XRDS_handler:
     def __init__(self, filename):
+
+        if filename is None:
+            raise ValueError
+
         self.netcdf_file = filename
         self.xrds = xr.open_dataset(self.netcdf_file)
 
@@ -11,5 +15,11 @@ class XRDS_handler:
     def get_attrs(self):
         return self.attributes
     
-    def get_variables(self):
-        return self.variables
+    def get_variable_names(self):
+        return [key for key in self.variables]
+
+    def get_latlon_matrix_at_given_time(self, variable, given_time):
+        if given_time is None or variable is None:
+            raise ValueError
+        
+        return self.xrds[variable].sel(time=given_time)
