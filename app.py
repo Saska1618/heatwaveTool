3edd2f83f09c97bd1c3e28350093bce584e9c801
Magyari-Sáska Attila <atti2002@gmail.com>
2024-08-19 14:06:39 +0300

    First version of file upload works

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 from shiny.express import ui, input
-from shiny import render, reactive
+from shiny import render, reactive, req
 from datetime import datetime
 from xrds_handler import XRDS_handler
 import plotly.express as px
 import pandas as pd
 import numpy as np
 import rasterio
-from rasterio.enums import Resampling
+# from rasterio.enums import Resampling
 from rasterio.transform import from_origin
-from rasterio.io import MemoryFile
+# from rasterio.io import MemoryFile
 import tempfile
 import os
 
-import base64
-
-import xarray as xr
+# import xarray as xr
 
 from shinywidgets import render_widget
 
-from ipyleaflet import Map, TileLayer, ImageOverlay
+from ipyleaflet import Map#, TileLayer, ImageOverlay
 from localtileserver import TileClient, get_leaflet_tile_layer
-import rioxarray
+# import rioxarray
 
 import matplotlib.pyplot as plt
 
@@ -28,35 +26,43 @@ ui.page_opts(fillable=True)
 
 ui.input_dark_mode()
 
-ds = XRDS_handler("./data/tg_ens_mean_0.1deg_reg_2011-2023_v29.0e.nc")
-#ds = None
+#ds = reactive.Value(XRDS_handler("./data/tg_ens_mean_0.1deg_reg_2011-2023_v29.0e.nc"))
+#ds = reactive.Value(None)
 
+@reactive.calc
+def get_ds():
+    ds = req(input.fileChosen())
+    return XRDS_handler('./data/' + ds[0]['name'])
 
 with ui.navset_card_pill(id="tab"):
 
     ### MAP FUNCTIONAL PANEL ###
 
+    with ui.nav_panel("Upload a file"):
+
+        with ui.card():
+                    
+            "Possible description"
+
+            ui.input_file("fileChosen", "Browse a file", accept=[".nc"], multiple=False)
+                    
+
     with ui.nav_panel("Climate data on Map"):
         with ui.layout_columns(col_widths=(4, 8)):
             with ui.card():
                 "Functional panel"
+                
                 with ui.card():
 
-                    ui.input_file("fileChosen_map", "Browse a file", accept=[".nc"], multiple=False)
+                    @render.ui
+                    def radvars_map():
+                        return ui.input_radio_buttons(
+                                "radio_variables_map",
+                                "Variables:",
+                                {var:var for var in get_ds().get_variable_names()}
+                            )
 
                 with ui.card():
-
-                    if ds is not None:
-                        ui.input_radio_buttons(
-                            "radio_variables_map",
-                            "Variables:",
-                            {var:var for var in ds.get_variable_names()}
-                        )
-                    else:
-                        "No variables"
-
-                with ui.card():
-
                     ui.input_date("date_map", "Date", value='2012-01-01') 
 
                 ### MAP OUTPUT ###
@@ -64,14 +70,11 @@ with ui.navset_card_pill(id="tab"):
             with ui.card():
                 
                 @render_widget  
+                # @reactive.event(input.fileChosen_map)
                 def map():
 
-                    if ds is None:
-                        return Map(zoom=3.5)
-
                     date_str_value = input.date_map().strftime('%Y-%m-%d')
-                    print(f"DDDAAATTEEE VALUE: {date_str_value}")
-                    data_array = ds.get_latlon_matrix_at_given_time(input.radio_variables_map(), date_str_value)
+                    data_array = get_ds().get_latlon_matrix_at_given_time(input.radio_variables_map(), date_str_value)
 
 
                     if data_array.rio.crs is None:
@@ -119,69 +122,56 @@ with ui.navset_card_pill(id="tab"):
             ### SETTINGS PANEL ###
             with ui.card():
                 "Functional panel"
-                with ui.card():
-
-                    ui.input_file("fileChosen_graph", "Browse a file", accept=[".nc"], multiple=False)
-
+                
                 with ui.card():
 
 
-                    if ds is not None:
-                        ui.input_radio_buttons(
-                            "radio_variables_graph",
-                            "Variables:",
-                            {var:var for var in ds.get_variable_names()}
-                        )
-                    else:
-                        "No variables"    
+                    @render.ui
+                    def radvars_graph():
+                        return ui.input_radio_buttons(
+                                "radio_variables_graph",
+                                "Variables:",
+                                {var:var for var in get_ds().get_variable_names()}
+                            )
+ 
 
                 with ui.card():
 
                     @render.ui
                     def input_date_range():
+                        start_date = datetime.fromtimestamp(int(get_ds().xrds[input.radio_variables_graph()]['time'].min().item() / 1e9)).strftime("%Y-%m-%d")
+                        end_date = datetime.fromtimestamp(int(get_ds().xrds[input.radio_variables_graph()]['time'].max().item() / 1e9)).strftime("%Y-%m-%d")
 
-                        if ds is not None:
-                            start_date = datetime.fromtimestamp(int(ds.xrds[input.radio_variables_graph()]['time'].min().item() / 1e9)).strftime("%Y-%m-%d")
-                            end_date = datetime.fromtimestamp(int(ds.xrds[input.radio_variables_graph()]['time'].max().item() / 1e9)).strftime("%Y-%m-%d")
-
-                            return ui.input_date_range("daterange_graph", "Date range", start=start_date, end=end_date)
-                        return ui.p("No file given")
+                        return ui.input_date_range("daterange_graph", "Date range", start=start_date, end=end_date)
 
                 with ui.card():
-                    if ds is not None:
-                        "Coordinates"
+                    "Coordinates"
 
-                        @render.ui
-                        def min_max_lat():
-                            min_lat = min(ds.xrds[input.radio_variables_graph()]['latitude']).values.item()
-                            max_lat = max(ds.xrds[input.radio_variables_graph()]['latitude']).values.item()
+                    @render.ui
+                    def min_max_lat():
+                        min_lat = min(get_ds().xrds[input.radio_variables_graph()]['latitude']).values.item()
+                        max_lat = max(get_ds().xrds[input.radio_variables_graph()]['latitude']).values.item()
 
-                            return ui.input_numeric("inp_lat_graph", "Latitude", 45, min=min_lat, max=max_lat)
-                        
-                        @render.ui
-                        def min_max_lon():
-                            min_lon = min(ds.xrds[input.radio_variables_graph()]['longitude']).values.item()
-                            max_lon = max(ds.xrds[input.radio_variables_graph()]['longitude']).values.item()
+                        return ui.input_numeric("inp_lat_graph", "Latitude", 45, min=min_lat, max=max_lat)
+                    
+                    @render.ui
+                    def min_max_lon():
+                        min_lon = min(get_ds().xrds[input.radio_variables_graph()]['longitude']).values.item()
+                        max_lon = max(get_ds().xrds[input.radio_variables_graph()]['longitude']).values.item()
 
-                            return ui.input_numeric("inp_lon_graph", "Longitude", 26, min=min_lon, max=max_lon)
+                        return ui.input_numeric("inp_lon_graph", "Longitude", 26, min=min_lon, max=max_lon)
 
-                    else:
-                        "No file given"
 
             ### GRAPH OUTPUT ###
 
             with ui.card():
+                @render_widget
+                def valChangeOnTime():
+                    gt = get_ds().get_ds_at_spec_latlon(input.radio_variables_graph(), lat=input.inp_lat_graph(), lon=input.inp_lon_graph(), start=input.daterange_graph()[0], end=input.daterange_graph()[1])
+                    df = gt.to_dataframe(name='value').drop(columns=['longitude', 'latitude'])
 
-                if ds is not None:
-                    @render_widget
-                    def valChangeOnTime():
-                        gt = ds.get_ds_at_spec_latlon(input.radio_variables_graph(), lat=input.inp_lat_graph(), lon=input.inp_lon_graph(), start=input.daterange_graph()[0], end=input.daterange_graph()[1])
-                        df = gt.to_dataframe(name='value').drop(columns=['longitude', 'latitude'])
+                    df['value'] = pd.to_numeric(df['value'], downcast='float')
 
-                        df['value'] = pd.to_numeric(df['value'], downcast='float')
-
-                        fig = px.scatter(df, x=df.index, y='value', title=f"{input.radio_variables_graph()} change over time at Lat: {input.inp_lat_graph()}, Lon: {input.inp_lon_graph()}", render_mode='webgl')
-                        return fig
-                else:
-                    "No file given"
+                    fig = px.scatter(df, x=df.index, y='value', title=f"{input.radio_variables_graph()} change over time at Lat: {input.inp_lat_graph()}, Lon: {input.inp_lon_graph()}", render_mode='webgl')
+                    return fig
                 
